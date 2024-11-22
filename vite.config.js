b012e7a612527ac43d99db7dc69e18c807909017
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2024-11-22 20:56:22 +0100

    prob 3

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/goit-react-hw-05/',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        404: './index.html',
-      },
-    },
+    sourcemap: true,
   },
 });
