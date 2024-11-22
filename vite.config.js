7d5bc96bf8fca60ea4c96daaf8be791f4b6e85e2
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2024-11-22 21:11:08 +0100

    pro4

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Базовый путь для Vercel
   build: {
-    sourcemap: true,
+    outDir: 'dist', // Папка для сборки
   },
 });
